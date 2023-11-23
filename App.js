3f7df3dfa312a478aa089b99199743d29ab0a6c2
Suraj Azad <surajkantazad@gmail.com>
2023-11-23 18:38:40 +0530

    update color

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Gifting Idea - Innovation Hubba 2023</h1>
+        <p>Building the right idea.....</p>
       </header>
     </div>
   );
